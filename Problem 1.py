62132ef7fe74697c1b0c7328ef1ab55436359cb0
Khansa-Gulshad <64579838+Khansa-Gulshad@users.noreply.github.com>
2021-10-27 15:10:22 +0800

    Update and rename Problem 1.md to Problem 1.py

--- Problem 1.py
+++ Problem 1.py
@@ -1,13 +1,13 @@
-### Python If-Else
-### Task
+Python If-Else
+Task
 Given an integer, n , perform the following conditional actions:
 1. If n is odd, print Weird
 2. If n is even and in the inclusive range of to , print Not Weird
 3. If n is even and in the inclusive range of to , print Weird
 4. If n is even and greater than , print Not Weird
 
-### Solution
-```ruby
+Solution
+
 n = int(input())
 if n % 2 != 0:
     print("Weird")
@@ -17,4 +17,4 @@ elif  n %2==0 and 6<= n <=20:
     print('Weird')
 elif n %2==0 and n >20:
     print('Not Weird')
-```
+
