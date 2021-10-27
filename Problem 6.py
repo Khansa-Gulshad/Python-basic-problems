3dc47e09d82624396ef489a3dbb360bc056d6466
Khansa-Gulshad <64579838+Khansa-Gulshad@users.noreply.github.com>
2021-10-27 15:21:55 +0800

    Update Problem 6.py

--- Problem 6.py
+++ Problem 6.py
@@ -1,10 +1,10 @@
 #Print Function
 #Task
 # Without using any string methods, try to print the following:
-# 123...n\
+# 123...n
 # Note that "....." represents the consecutive values in between.
 # Example
-# n=5\
+# n=5
 # Print the string 12345.
 #Solution
 
