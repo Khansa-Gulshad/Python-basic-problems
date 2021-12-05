bb4ea2fae5862e4da738bba96040242eeaa7f95c
Khansa-Gulshad <64579838+Khansa-Gulshad@users.noreply.github.com>
2021-12-05 21:22:41 +0800

    Create Problem 22.py

---
+++ Problem 22.py
@@ -0,0 +1,6 @@
+# You are given two integer arrays of size NXP and MXP (N & M are rows, and P is the column). Your task is to concatenate the arrays along axis 0.
+# The first line contains space separated integers N ,M and P.
+# The next N lines contains the space separated elements of the P columns. 
+# After that, the next M lines contains the space separated elements of the P columns.
+# Print the concatenated array of size (N+M)XP.
+
