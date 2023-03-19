0440b7daade48d5036ed9253d043abe02713501b
Khansa-Gulshad <64579838+Khansa-Gulshad@users.noreply.github.com>
2023-03-19 16:18:19 +0100

    Update Problem 5.py

--- Problem 5.py
+++ Problem 5.py
@@ -28,3 +28,14 @@ is_leap(year)
 def is_leap(year):
     return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
 
+#or by this method
+ def is_leap(year):
+    leap = False
+    if year%4==0 and year%100 !=0:
+        return True
+    elif year%100 ==0 and year%400 !=0:
+        return False
+    elif year%400 ==0:
+        return True
+    return leap
+
